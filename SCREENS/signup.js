2a20358290fdac266accee74e4a7d2862c374d88
Muhammad Raza <103913857+mohammadraza123@users.noreply.github.com>
2023-02-25 22:39:51 +0500

    Update signup.js

--- SCREENS/signup.js
+++ SCREENS/signup.js
@@ -1,12 +1,23 @@
-import {View, Text,Image,StyleSheet,Pressable, } from "react-native"
-const signup =()=> {
+
+import {View, Text,Image,StyleSheet,Pressable, TextInput} from "react-native"
+// import email from "../assests/Email icon.png"
+
+
+const welcome =()=> {
   return (
     <View style={styles.container}>
     
-      <Image  source={require("../assests/Logo.png")} />
+     
+      <Image  source={require("../assests/Email icon.png")} />
       <Text style={styles.heading} >SAYLANI WELFARE</Text>
       <Text style={styles.second}>Online Discount Store</Text>
-     <Pressable style={{
+      {/* <email/> */}
+     <TextInput style={styles.userName}  placeholder="Full Name"></TextInput>
+     <TextInput style={styles.userName}  placeholder="Full Name"></TextInput>
+     <TextInput style={styles.userName}  placeholder="Full Name"></TextInput>
+     <TextInput style={styles.userName}  placeholder="Full Name"></TextInput>
+
+     <Pressable  style={{
       backgroundColor: '#61B842',
       fontWeight : 'bold',
       marginTop : 190, 
@@ -21,7 +32,7 @@ const signup =()=> {
 const styles = StyleSheet.create({
   container: {
     margin : 'auto',
-    marginTop: 50,
+    marginTop: 30,
   
     alignItems : 'center', 
   },
@@ -29,7 +40,7 @@ const styles = StyleSheet.create({
     fontSize : 30,
     fontWeight : 'bold',
     color : '#61B846',
-    marginTop : 20
+    
   },
   second : {
     color : 'blue',
@@ -40,9 +51,66 @@ const styles = StyleSheet.create({
     fontSize : 20,
     color : 'red',
   },
+  userName:{
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width : 250,
+  }
   
  
 });
 
 
-export default signup;
+export default welcome;
+
+
+// import {View, Text,Image,StyleSheet,Pressable, } from "react-native"
+// const signup =()=> {
+//   return (
+//     <View style={styles.container}>
+    
+//       <Image  source={require("../assests/Logo.png")} />
+//       <Text style={styles.heading} >SAYLANI WELFARE</Text>
+//       <Text style={styles.second}>Online Discount Store</Text>
+//      <Pressable style={{
+//       backgroundColor: '#61B842',
+//       fontWeight : 'bold',
+//       marginTop : 190, 
+//       padding : 10,
+//       borderRadius : 12,
+//       width : 180}}>
+//         <Text style = {{fontSize : 25 , textAlign : 'center'}}>Get Started</Text></Pressable>
+//      </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     margin : 'auto',
+//     marginTop: 50,
+  
+//     alignItems : 'center', 
+//   },
+//   heading : {
+//     fontSize : 30,
+//     fontWeight : 'bold',
+//     color : '#61B846',
+//     marginTop : 20
+//   },
+//   second : {
+//     color : 'blue',
+//     fontSize : 17,
+//   },
+//   button : {
+//     marginBottom : 50,
+//     fontSize : 20,
+//     color : 'red',
+//   },
+  
+ 
+// });
+
+
+// export default signup;
